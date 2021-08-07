7e5a235e337e01d4f479eb852a58a827f8aecac5
mumu <tnddls2ek@naver.com>
2021-08-07 19:28:03 +0900

    UPDATE - 검색 결과 사진 앨범에 저장 기능 추가

--- ImSearch/Controllers/DetailViewController.swift
+++ ImSearch/Controllers/DetailViewController.swift
@@ -16,6 +16,7 @@ protocol DetailViewControllerDelegate {
 
 //MARK: - Controller
 class DetailViewController: UIViewController {
+    
     @IBOutlet weak var detailImageView: UIImageView!
     var detailImageURL: String?
     var navigationTitle: String?
@@ -30,6 +31,35 @@ class DetailViewController: UIViewController {
         
         loadDetailImage()
         setNavigationTitle()
+    }
+    
+    @IBAction func saveImageToAlbum(_ sender: Any) {
+        // 이미지를 사진첩에 저장
+        //TODO: 저장할건지 여부 물어보자
+        guard let image = self.detailImageView.image else {
+            print("이미지를 로드할 수 없뜸니다 ,,,")
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(setImage(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    @objc func setImage(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+           //사진 저장 한후
+           if let error = error {
+               // we got back an error!
+            print("에러처리 아몰랑 ~ \(error.localizedDescription)")
+           } else {
+                // save
+            completeAlert()
+           }
+    }
+    func completeAlert(){
+        let alert = UIAlertController(title: "알림", message: "저장이 완료되었습니다.", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "넹", style: .default) { (ok) in
+           print("그래 그래~")
+        }
+        alert.addAction(ok)
+        
+        self.present(alert, animated: true, completion: nil)
     }
 }
 //MARK: - DetailView Delegate
